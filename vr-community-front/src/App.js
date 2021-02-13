e596eb5738e876dc9744f3c3491862f8b444a8c1
irostub <irostub@gmail.com>
2021-02-13 23:20:08 +0900

    Implement routing component

--- vr-community-front/src/App.js
+++ vr-community-front/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
+import NavigationComponent from "./components/NavigationComponent/NavigationComponent";
+import FooterComponent from "./components/FooterComponent/FooterComponent";
+
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import NewComponent from "./components/NewsComponent";
+import VideoComponent from "./components/VideoComponent";
+import GameInfoComponent from "./components/GameInfoComponent";
+import CommunityComponent from "./components/CommunityComponent";
+import ProductComponent from "./components/ProductComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <HeaderComponent />
+      <NavigationComponent />
+      <Switch>
+        <Route path="/" exact component={NewComponent}></Route>
+        <Route path="/news" component={NewComponent}></Route>
+        <Route path="/video" component={VideoComponent}></Route>
+        <Route path="/gameinfo" component={GameInfoComponent}></Route>
+        <Route path="/Community" component={CommunityComponent}></Route>
+        <Route path="/Product" component={ProductComponent}></Route>
+      </Switch>
+      <FooterComponent />
+    </Router>
   );
 }
 
